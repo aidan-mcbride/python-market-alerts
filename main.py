3f8fc27d9631071c5a947834f263af9c6feb5f76
aidan mcbride <aidanmcbride@outlook.com>
2019-09-13 13:30:46 -0600

    Add functions to retrieve silver price with bs4

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def hello_world():
-    return "Hello, World!"
+"""
+Entire Application is in this file.
+
+Because the whole thing is really quite simple.
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_page_content(url: str) -> str:
+    """retrieve html from a given url"""
+    rv = requests.get(url)
+    html: str = rv.text
+    return html
+
+
+def html_to_soup(html: str) -> BeautifulSoup:
+    """turns a string of HTML into a soup"""
+    return BeautifulSoup(html, "html.parser")
+
+
+def extract_silver_price(soup: BeautifulSoup) -> float:
+    """extracts the price of silver as a float from a soup"""
+    result = soup.find("div", {"id": "sounce"})
+    price = float(result.string)
+    return price
 
 
 if __name__ == "__main__":
-    print(hello_world())
+    """
+    Run program
+    """
+    url = "https://www.jmbullion.com/charts/silver-prices/"
+
+    html = get_page_content(url)
+    soup = html_to_soup(html)
+    silver_price = extract_silver_price(soup)
+    print(silver_price)
