ec0026ee0490fe69a9d0c63cff038e0e8355e074
aidan mcbride <aidanmcbride@outlook.com>
2019-09-16 08:51:34 -0600

    Move defaults to global dictionary

--- cli.py
+++ cli.py
@@ -2,29 +2,35 @@ import click
 
 import main
 
-# hard-coded global source,
-# since right now there is no support for scraping multiple sources
-source = "https://www.jmbullion.com/charts/silver-prices/"
+defaults = dict(
+    source="https://www.jmbullion.com/charts/silver-prices/",
+    interval=900,
+    limit=1.00,
+    side="above",
+)
 
 
 @click.command()
 @click.option(
     "--interval",
-    default=900,
+    default=defaults["interval"],
     help="Number of seconds to wait between checking price. Default is 15 minutes(900 seconds)",
 )
-@click.option("--limit", default=1.00, help="Price at which to be notified")
+@click.option(
+    "--limit",
+    default=defaults["limit"],
+    help="Spot price of silver at which to be notified",
+)
 @click.option(
     "--side",
-    default="above",
+    default=defaults["side"],
     help="either 'above' or 'below' - check that current price is above or below the given limit",
 )
-def run(interval: int, limit: float, side: str):
+def run(interval: int, limit: float, side: str, source: str = defaults["source"]):
     """
     take inputs via CLI options
     launch application with given options
     """
-    global source
     main.check_price_periodically(
         interval=interval, source=source, limit=limit, side=side
     )
